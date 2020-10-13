7f3b89df3b45ada02d3744abd404fff35a138f68
李世星 <70449@any3.com>
2020-10-13 15:51:22 +0800

    不加drag可正常编译和播放

--- app/src/main/java/media/ushow/as_video_player/MainActivity.java
+++ app/src/main/java/media/ushow/as_video_player/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.xingfeng.sxliveplayer.ELLivePlayerController;
+import com.xingfeng.sxliveplayer.SpeedUpFirstScreenPlayerActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
     private Button forward_video_player;
+    private Button live_video_player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        live_video_player = (Button) findViewById(R.id.live_video_player);
+        live_video_player.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String path = "rtmp://58.200.131.2:1935/livetv/hunantv";
+                ELLivePlayerController.getInstance().init(path, true);
+                Intent intent = new Intent(MainActivity.this, SpeedUpFirstScreenPlayerActivity.class);
+                startActivity(intent);
+            }
+        });
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             this.requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 10);
